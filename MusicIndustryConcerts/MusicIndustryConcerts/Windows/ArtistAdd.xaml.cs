91617422eb5cddbced295e9f74217c42200699df
miceyjuice <ganjafz@gmail.com>
2021-01-18 16:08:07 +0100

    Added basic pages for the app

---
+++ MusicIndustryConcerts/MusicIndustryConcerts/Windows/ArtistAdd.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicIndustryConcerts.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ArtistAdd.xaml
+    /// </summary>
+    public partial class ArtistAdd : Page
+    {
+        public ArtistAdd()
+        {
+            InitializeComponent();
+            FillGenres();
+        }
+
+        private void AddArtist()
+        {
+            var context = new MusicIndustryConcertsEntities();
+
+            var newArtist = new Artists() {
+                ArtistName = artistNameInput.Text,
+                MusicGenre = artistMusicGenreInput.SelectedValue.ToString(),
+                PerformancePrice = Convert.ToInt32(artistPerformancePriceInput.Text),
+                ExplicitContent = artistExplicitContentInput.IsChecked ?? false,
+                ArtistAvailability = artistAvailabilityInput.IsChecked ?? false
+            };
+
+            context.Artists.Add(newArtist);
+            context.SaveChanges();
+        }
+
+        private void FillGenres()
+        {
+            var genreList = new List<string>();
+
+            artistMusicGenreInput.Items.Add("Metal");
+            artistMusicGenreInput.Items.Add("Pop");
+            artistMusicGenreInput.Items.Add("Rap");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddArtist();
+        }
+    }
+}
