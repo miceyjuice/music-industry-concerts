321943298ffdfc404b4c2244bcaebb891b52a06f
miceyjuice <ganjafz@gmail.com>
2021-01-31 21:01:51 +0100

    pages and basic main menu view added

--- MusicIndustryConcerts/MusicIndustryConcerts/Windows/PlaceAdd.xaml.cs
+++ MusicIndustryConcerts/MusicIndustryConcerts/Windows/PlaceAdd.xaml.cs
@@ -31,14 +31,29 @@ namespace MusicIndustryConcerts.Windows
             AddPlace();
         }
 
+        private void Button_ClickBack(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("I GO BACK");
+        }
+
         private void AddPlace()
         {
             var context = new MusicIndustryConcertsEntities();
 
             var newPlace = new Places()
             {
-                PlaceName = 
-            }
+                PlaceName = placeNameInput.Text,
+                Capacity = Int32.Parse(placeCapacityInput.Text),
+                BarArea = placeBarAreaCheckBox.IsChecked ?? false,
+                VIPArea = placeVipAreaCheckBox.IsChecked ?? false,
+                OpeningHour = Int32.Parse(placeOpeningHourInput.Text),
+                ClosingHour = Int32.Parse(placeClosingHourInput.Text),
+                AdultsOnly = placeAdultsOnlyCheckBox.IsChecked ?? false,
+                RentalPrice = Int32.Parse(placeRentalPriceInput.Text)
+            };
+
+            context.Places.Add(newPlace);
+            context.SaveChanges();
         }
     }
 }
