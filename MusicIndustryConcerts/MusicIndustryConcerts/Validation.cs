38a343136ab61f392d3c25d2355d9766cf704fbc
miceyjuice <ganjafz@gmail.com>
2021-02-04 17:03:14 +0100

    seperate class for validation

---
+++ MusicIndustryConcerts/MusicIndustryConcerts/Validation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MusicIndustryConcerts
+{
+    public class Validation
+    {
+
+        private bool ValidateTextBox(TextBox field)
+        {
+            if (String.IsNullOrWhiteSpace(field.Text))
+            {
+                field.BorderBrush = Brushes.Red;
+                return false;
+            }
+            return true;
+        }
+
+        private static  bool ValidateComboBox(ComboBox field)
+        {
+            //TODO Validate for a custom style
+            if (field.SelectedIndex == -1) return false;
+            return true;
+        }
+
+        private bool ValidateDateInput(DatePicker field)
+        {
+            //TODO Validate for a custom style
+            if (field.SelectedDate == null) return false;
+            return true;
+        }
+
+        public bool ValidateFields(Control[] controls)
+        {
+            bool isAllValidated = true;
+            foreach (var control in controls)
+            {
+                if (typeof(TextBox).IsInstanceOfType(control))
+                {
+                    isAllValidated = ValidateTextBox((TextBox)control) ? isAllValidated : false;
+                }
+                else if (typeof(ComboBox).IsInstanceOfType(control))
+                {
+                    isAllValidated = ValidateComboBox((ComboBox)control) ? isAllValidated : false;
+                }
+                else if (typeof(DatePicker).IsInstanceOfType(control))
+                {
+                    isAllValidated = ValidateDateInput((DatePicker)control) ? isAllValidated : false;
+                }
+                else throw new ArgumentException("Type of the argument is not supported");
+            }
+            return isAllValidated;
+        }
+    }
+}
